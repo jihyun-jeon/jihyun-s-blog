abf6dadd49a98ae18897c3e73d36c0dec3d9f46b
jihyun-jeon <jhplus13@naver.com>
2024-12-26 23:18:17 +0900

    chore: 타입변수명 변경

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-import { Post } from "@/type";
+import { PostType } from "@/type";
 import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
@@ -15,10 +15,10 @@ export function getPostBySlug(slug: string) {
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
-  return { ...data, slug: realSlug, content } as Post;
+  return { ...data, slug: realSlug, content } as PostType;
 }
 
-export function getAllPosts(): Post[] {
+export function getAllPosts(): PostType[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
