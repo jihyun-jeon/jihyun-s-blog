648660a93e8305f1a7dac992d4f3dfc9a425549f
jihyun-jeon <jhplus13@naver.com>
2025-01-24 15:13:54 +0900

    feat :  mdx 파일기반 라우팅,mdx 렌더

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"], //  .md 및 .mdx 파일이 애플리케이션에서 페이지, 경로 또는 가져오기 역할을 할 수 있습니다.
+  // Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
