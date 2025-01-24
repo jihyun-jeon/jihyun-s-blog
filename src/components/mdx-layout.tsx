fcc309cd72c7f91a6e3bb55252d866b42cb2624c
jihyun-jeon <jhplus13@naver.com>
2025-01-24 17:56:07 +0900

    feat: prismjs를 활용한 코드 스타일 처리

--- src/components/mdx-layout.tsx
+++ src/components/mdx-layout.tsx
@@ -1,14 +1,21 @@
 import Footer from "./Footer";
 import Headers from "./Headers";
+import SyntaxHighlighter from "./SyntaxHighlighter";
 import Thumbnail from "./Thumbnail";
 
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
+  const isProduction = process.env.NODE_ENV === "production";
+
   return (
     <div>
       <Headers />
       <div className="px-5 md:px-0 max-w-4xl mx-auto prose dark:prose-invert">
         <Thumbnail />
-        {children}
+        {isProduction ? (
+          <SyntaxHighlighter>{children}</SyntaxHighlighter>
+        ) : (
+          <div>{children}</div>
+        )}
       </div>
       <Footer />
     </div>
